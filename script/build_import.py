e9b793283c4738890d4d77e5b295f4fb34fe7b7d
François Boiteux <francois.boiteux@epita.fr>
2015-06-11 19:29:06 +0200

    [DATA] Sql script to import openData RATP full database

---
+++ script/build_import.py
@@ -0,0 +1,22 @@
+import codecs
+import locale
+import csv
+
+with codecs.open('data.sql', 'w', encoding='utf8') as dataFile:
+    dataFile.write('USE ratp;\n')
+    with codecs.open('data/line_utf8.csv', 'r') as lineFile:
+        readerCsv = csv.reader(lineFile, delimiter=';')
+        for row in readerCsv:
+        	dataFile.write('INSERT INTO line(id_line, line_number, line_name) VALUES (\'' + row[0] + '\',\'' + row[1] + '\',"' + row[2].decode('utf8') + '");\n')
+
+    with codecs.open('data/station_utf8.csv', 'r') as lineFile:
+        readerCsv = csv.reader(lineFile, delimiter=';')
+        for row in readerCsv:
+        	dataFile.write('INSERT INTO station(id_station, station_name) VALUES (\'' + row[0] + '\',"' + row[1].decode('utf8') + '");\n')
+
+    with codecs.open('data/line_station_utf8.csv', 'r') as lineFile:
+        readerCsv = csv.reader(lineFile, delimiter=';')
+        for row in readerCsv:
+        	dataFile.write('INSERT INTO line_station(id_line, id_station) VALUES (\'' + row[0] + '\',\'' + row[1] + '\');\n')
+
+
--- script/db.sql
+++ script/db.sql
@@ -1,19 +1,20 @@
-create database ratp
+DROP DATABASE IF EXISTS ratp;
+CREATE DATABASE ratp;
 
-use ratp
+USE ratp;
 
 CREATE TABLE users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(50) NOT NULL,
 password VARCHAR(50) NOT NULL,
 token VARCHAR(50) NOT NULL
-)
+);
 
 CREATE TABLE offers (
 id INT(6) UNSIGNED auto_increment PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 max_queries INT(6) UNSIGNED NOT NULL
-)
+);
 
 CREATE TABLE comsumption (
 user INT(6) UNSIGNED NOT NULL,
@@ -21,4 +22,22 @@ offer INT(6) UNSIGNED NOT NULL,
 datetimestamp datetime NOT NULL,
 FOREIGN KEY (user) REFERENCES users(id),
 FOREIGN KEY (offer) REFERENCES offers(id)
-)
+);
+
+CREATE TABLE line (
+id_line INT(6) UNSIGNED PRIMARY KEY,
+line_number VARCHAR(3) NOT NULL,
+line_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE station (
+id_station INT(6) UNSIGNED PRIMARY KEY,
+station_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE line_station(
+id_line INT(6) UNSIGNED NOT NULL,
+id_station INT(6) UNSIGNED NOT NULL,
+FOREIGN KEY(id_line) REFERENCES line(id_line),
+FOREIGN KEY(id_station) REFERENCES station(id_station)
+);
