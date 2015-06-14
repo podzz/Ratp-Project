ab3f27faca5c35a84b629beebcc61cae3fce197f
Edouard Durieux <edouard.durieux@gmail.com>
2015-06-15 00:02:00 +0200

    Updated oauth token request

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -6,8 +6,9 @@ return new \Phalcon\Config(array(
     'database' => array(
         'adapter'     => 'Mysql',
         'host'        => '127.0.0.1',
+        'port'        => '3306',
         'username'    => 'root',
-        'password'    => 'Password',
+        'password'    => '',
         'dbname'      => 'ratp',
         "options" => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
