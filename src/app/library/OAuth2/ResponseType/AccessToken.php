58dfd282a1d06f1b87de6d9cf787e1962e64c4a0
Edouard Durieux <edouard.durieux@gmail.com>
2015-06-17 21:23:33 +0200

    Changed token lifetime

--- src/app/library/OAuth2/ResponseType/AccessToken.php
+++ src/app/library/OAuth2/ResponseType/AccessToken.php
@@ -34,7 +34,7 @@ class AccessToken implements AccessTokenInterface
 
         $this->config = array_merge(array(
             'token_type'             => 'bearer',
-            'access_lifetime'        => 31536000,
+            'access_lifetime'        => 3600,
             'refresh_token_lifetime' => 1209600,
         ), $config);
     }
